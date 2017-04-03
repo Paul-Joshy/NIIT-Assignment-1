8298106465746d2ac7b9b9b54bc5156a7427ad43
Paul Joshy <pauljoshyk96@gmail.com>
2017-04-03 11:44:25 +0530

    Added files

---
+++ js/code.js
@@ -0,0 +1,10 @@
+$('.carousel').carousel();
+
+var calculate = function()
+{
+    var a = parseInt(prompt("Enter first no"));
+    var b = parseInt(prompt("Enter second no"));
+    var sum = a+b;
+    alert("Sum =" + sum);
+}
+
